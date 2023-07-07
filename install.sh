711b111a175eae882efec59b2be88c5fbb6a2193
Xgame <104642035+Xgameisdabest@users.noreply.github.com>
2023-07-07 09:26:54 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+#install python
+sudo apt upgrade
+sudo apt install python3
+sudo apt install python3-pip
+pip install tqdm
+
+#install Xos
+git clone https://github.com/Xgameisdabest/X-operating-system--in-beta-.git
